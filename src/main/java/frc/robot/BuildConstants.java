8e32a4d8afdda7d82b60177941c8f7d649c58c16
Bilol.B <113650175+Bilol135@users.noreply.github.com>
2025-10-15 18:37:37 -0400

    first commit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "AdvantageKit_SparkSwerveTemplate";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2025-10-15 17:47:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1760564862237L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants() {}
+}
